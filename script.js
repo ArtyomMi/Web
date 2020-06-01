03a3c0e27d0caa5d9022e07510c6fe6f44f96d9d
artemiy.mitrofanov <artemiy.mitrofanov@mail.ru>
2020-06-01 19:26:48 +0300

    Create JS function

---
+++ script.js
@@ -0,0 +1,20 @@
+var btn = document.getElementById("Test");
+
+function show(){
+    var a = document.getElementById("navi");
+    if(a.hidden){
+        btn.textContent = "Скрыть панель навигации";
+        a.hidden=false;
+    }
+    else{
+    a.hidden = true;
+    btn.textContent = "Показать панель навигации";
+        
+    }
+    // console.log(a);
+}
+
+// if(btn){
+btn.addEventListener("click", show);
+
+// }
