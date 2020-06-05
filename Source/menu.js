750c4b25f2e027c4bea8b45dbf9b51ec6624e686
artemiy.mitrofanov <artemiy.mitrofanov@mail.ru>
2020-06-05 18:35:43 +0300

    commit

---
+++ Source/menu.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById("ShowLinks");
+
+function show() {
+    var a = document.getElementById("navi");
+    if (a.hidden) {
+        btn.textContent = "Скрыть панель навигации";
+        a.hidden = false;
+    } else {
+        a.hidden = true;
+        btn.textContent = "Показать панель навигации";
+
+    }
+    // console.log(a);
+}
+
+// if(btn){
+btn.addEventListener("click", show);
+
+// }
